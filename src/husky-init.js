8c5fbaaa08c08096084a69054f9cc6aff1ab6487
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-06-30 14:36:57 +0800

    fix: fix workspace path and relative path
    - 脚本工作路径与文件路径问题修复

--- src/husky-init.js
+++ src/husky-init.js
@@ -1,11 +1,16 @@
+#! /usr/bin/env node
+
 const fs = require('fs');
+const path = require('path');
 const installPackages = require('./utils/install-packages');
 const jsonFormater = require('./utils/json-formater');
-
 const jsonStringify = jsonFormater(2);
+const workspace = process.cwd();
 
 installPackages(['husky@4.3.6'], ['-D']);
-const pkg = require('../package.json');
+const pkg = require(
+  path.resolve(workspace, 'package.json')
+);
 pkg.husky = {
   "hooks": {
     "commit-msg": "commitlint -e $GIT_PARAMS"
--- src/standard-version.js
+++ src/standard-version.js
@@ -1,11 +1,16 @@
+#! /usr/bin/env node
+
 const fs = require('fs');
 const installPackages = require('./install-packages');
 const jsonFormater = require('./json-formater');
-
+const path = require('path');
 const jsonStringify = jsonFormater(2);
+const workspace = process.cwd();
 
 installPackages(['standard-version'], ['-D']);
 
-const pkg = require('../package.json');
+const pkg = require(
+  path.resolve(workspace, 'package.json')
+);
 pkg.scripts.release = 'yarn standard-version';
 fs.writeFileSync('./package.json', jsonStringify(pkg));
